0749ec7dafc5486c86c79e131916dd92ce3d7d7f
Jenny Aguilar <contact@jaguilarweb.com>
2022-05-26 10:08:11 +0200

    Refactor and include HTTP service to retrive data

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,24 +3,24 @@ import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
+  private heroesUrl = 'api/heroes'; //URL to web api
+
   constructor(
+    private http: HttpClient,
     private messageService: MessageService
   ) { }
 
-/*   getHeroes(): Hero[] {
-    return HEROES;
-  } */
+
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
 
   getHero(id: number): Observable<Hero> {
@@ -29,6 +29,11 @@ export class HeroService {
   const hero = HEROES.find(h => h.id === id)!;
   this.messageService.add(`HeroService: fetched hero id=${id}`);
   return of(hero)
+  }
+
+    /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 
 }
